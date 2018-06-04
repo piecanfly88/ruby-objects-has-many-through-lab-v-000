a0fadec27b53d84dbb265033db578158b71def10
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-04 22:24:17 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,4 +19,9 @@ class Doctor
   def appointments
     Appointment.all.select {|appointment| appointment.doctor == self}
   end
+
+  def patients
+    self.all.select {|appointment| appointment.patient}
+  end 
+  
 end
