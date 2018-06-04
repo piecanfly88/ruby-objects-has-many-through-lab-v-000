fd6152606b8d863f45b2b7c121e200402f57a2a9
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-04 21:04:00 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    @@all << Song.new(self,name, genre)
+  end
+end
