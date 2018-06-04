e1362bb74b02a332c35ae2b2faa8633bec485620
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-06-04 21:31:06 +0000

    automatically backed up by learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,15 @@
 class Genre
   attr_accessor :name
 
+  @@all = []
 
   def initialize(name)
     @name = name
+    @@all << self
+  end
+
+  def song_new(name, artist)
+    Song.new(name, artist, self)
   end
 
 end
